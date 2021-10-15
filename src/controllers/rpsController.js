fbb6a6ddbb44ea6297a3320c76f3e11cc2726c78
nei <nei.thomass@gmail.com>
2021-10-15 17:35:51 -0300

    implementa criar e atualizar dados

--- src/controllers/rpsController.js
+++ src/controllers/rpsController.js
@@ -6,7 +6,7 @@ const home = '<h2>Welcome at the home page from RPS API</h2>\
             <p>consultar uma: <a href="http://localhost:7575/enotafiscal/api/v1/rps/6165545caadd4e2bbadbe6f2">\
             http://localhost:7575/enotafiscal/api/v1/rps/id</a>\
             </p>';
-const urlResponse = 'http://localhost:7070/rpss/rps/created';
+const urlCreated = 'http://localhost:7070/rpss/rps/created';
 
 
 exports.index = (req, res, next) => {
@@ -20,7 +20,6 @@ exports.rpsListAll = (req, res, next) => {
 
         if(err) { return next(err); }
 
-        console.log('LIST ALL:', list_rps);
         res.status(200).json({ 
                 message: undefined===list_rps ? 'list undefined' : list_rps 
             });
@@ -40,8 +39,6 @@ exports.rpsListOne = (req, res, next) => {
 };
 
 exports.rpsCreate = (req, res, next) => {
-
-    console.log('REQ: \n', req.body);
     
     let rps = new RPS(
         {
@@ -102,106 +99,108 @@ exports.rpsCreate = (req, res, next) => {
                 inss: req.body['fisco-inss'],
                 irrf: req.body['fisco-irrf'],
                 outras: req.body['fisco-outras'],
-                iss: req.body.aliquotaiss['iss'],
+                iss: req.body['aliquotaiss'],
                 desconto: req.body['fisco-desconto']
             }],
-            valor: req.body['valor-rps'],
+            valor: req.body['valoraliquotaiss'],
             valorNota: req.body['valor-nota-rps'],
             issRetido: req.body['iss-retido'],
             outras: req.body.outras
         }
     );
-    console.log('RPS object:\n',rps);
     rps.save( (err) => {
 
         if(err) { return next(err); }
 
-        console.log('MESS: \n', rps);
-        res.json(urlResponse, { rps });
+        res.status(201).json(urlCreated, { rps });
     });
 };
 
 exports.rpsUpdate = (req, res, next) => {
+    console.log(req.body);
 
     let rps = 
         {
             dataHora: req.body.datahora,
             numero: req.body.numero,
             serie: req.body.serie,
-            prestador: 
+            prestador: [
             {
-                cpf: req.body.cpf-prestador,
-                cnpj: req.body.cnpj-prestador,
-                inscr_municipal: req.body.inscr-municipal-prestador,
-                nome_razao: req.body.nome-razao-prestador,
-                endereco: 
-                { 
-                    cep: req.body.end-cep-prestador,
-                    logradouro: req.body.end-logradouro-prestador,
-                    numero: req.body.end-numero-prestador,
-                    complemento: req.body.end-complemento-prestador,
-                    bairro: req.body.end-bairro-prestador,
-                    cidade: req.body.end-cidade-prestador,
-                    uf: req.body.end-uf-prestador,
-                },
-                fone: req.body.fone-prestador,
-                email: req.body.email-prestador
-            },
-            tomador: 
+                cpf: req.body['cpf-prestador'],
+                cnpj: req.body['cnpj-prestador'],
+                inscr_municipal: req.body['inscr-municipal-prestador'],
+                nome_razao: req.body['nome-razao-prestador'],
+                endereco: [
+                {
+                    cep: req.body['end-cep-prestador'],
+                    logradouro: req.body['end-logradouro-prestador'],
+                    numero: req.body['end-numero-prestador'],
+                    complemento: req.body['end-complemento-prestador'],
+                    bairro: req.body['end-bairro-prestador'],
+                    cidade: req.body['end-cidade-prestador'],
+                    uf: req.body['end-uf-prestador']
+                }],
+                fone: req.body['fone-prestador'],
+                email: req.body['email-prestador']
+            }],
+            tomador: [
             {
-                cpf: req.body.cpf-tomador,
-                cnpj: req.body.cnpj-tomador,
-                inscr_municipal: req.body.inscr-municipal-tomador,
-                nome_razao: req.body.nome-razao-tomador,
-                endereco: 
-                { 
-                    cep: req.body.end-cep-tomador,
-                    logradouro: req.body.end-logradouro-tomador,
-                    numero: req.body.end-numero-tomador,
-                    complemento: req.body.end-complemento-tomador,
-                    bairro: req.body.end-bairro-tomador,
-                    cidade: req.body.end-cidade-tomador,
-                    uf: req.body.end-uf-tomador
-                },
-                fone: req.body.fone-tomador,
-                email: req.body.email-tomador
-            },
-            cod_atividade: req.body.cod-atividade,
-            discriminacao: 
+                cpf: req.body['cpf-tomador'],
+                cnpj: req.body['cnpj-tomador'],
+                inscr_municipal: req.body['inscr-municipal-tomador'],
+                nome_razao: req.body['nome-razao-tomador'],
+                endereco:[
+                {
+                    cep: req.body['end-cep-prestador'],
+                    logradouro: req.body['end-logradouro-prestador'],
+                    numero: req.body['end-numero-prestador'],
+                    complemento: req.body['end-complemento-prestador'],
+                    bairro: req.body['end-bairro-prestador'],
+                    cidade: req.body['end-cidade-prestador'],
+                    uf: req.body['end-uf-prestador']
+                }],
+                fone: req.body['fone-tomador'],
+                email: req.body['email-tomador']
+            }],
+            cod_atividade: req.body['cod-atividade'],
+            discriminacao: [
             {
-                quantidade: req.body.discr-quantidade,
-                descricao: req.body.discr-descricao,
-                valorUnitario: req.body.discr-valor-uni,
-                valorTotal: req.body.discr-valor-total
-            },
-            fisco: 
+                quantidade: req.body['discr-quantidade'],
+                descricao: req.body['discr-descricao'],
+                valorUnitario: req.body['discr-valor-unitario'],
+                valorTotal: req.body['discr-valor-total']
+            }],
+            fisco: [
             {
-                pis: req.body.fisco-pis,
-                cofins: req.body.fisco-cofins,
-                csll: req.body.fisco-csll,
-                inss: req.body.fisco-inss,
-                irrf: req.body.fisco-irrf,
-                outras: req.body.fisco-outras,
-                iss: req.body.fisco-iss
-            },
-            valor: req.body.valor-rps,
-            issRetido: req.body.iss-retido
+                pis: req.body['fisco-pis'],
+                cofins: req.body['fisco-cofins'],
+                csll: req.body['fisco-csll'],
+                inss: req.body['fisco-inss'],
+                irrf: req.body['fisco-irrf'],
+                outras: req.body['fisco-outras'],
+                iss: req.body['aliquotaiss'],
+                desconto: req.body['fisco-desconto']
+            }],
+            valor: req.body['valoraliquotaiss'],
+            valorNota: req.body['valor-nota-rps'],
+            issRetido: req.body['iss-retido'],
+            outras: req.body.outras
         };
 
         RPS.findByIdAndUpdate(req.params.id, rps, {}, function (err, rps) {
     
             if(err) { return next(err); }
     
-            res.redirect(rps.url);
+            res.status(201).json(urlCreated, { rps });
         });
     };
 
 exports.rpsDelete = (req, res, next) => {
 
-    RPS.findByIdAndRemove(req.body.id, function (err) {
+    RPS.findByIdAndRemove(req.params.id, function (err) {
 
         if (err) { return next(err); }
         
-        res.redirect('/rps');
+        res.redirect(urlCreated, { rps });
     });
 };
